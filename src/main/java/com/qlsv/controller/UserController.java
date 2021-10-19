c5e93777513c79139e4a3345ac9d07b32d70da7f
MSI\Minh Tan <tanlun04112000@gmail.com>
2021-10-19 11:54:18 +0700

    config eureka userservice client

--- src/main/java/com/qlsv/controller/UserController.java
+++ src/main/java/com/qlsv/controller/UserController.java
@@ -4,11 +4,11 @@ import com.qlsv.entity.User;
 import com.qlsv.service.UserService;
 import com.qlsv.vo.ResponseTemplateVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-//@EnableEurekaClient
 public class UserController
 {
     @Autowired
@@ -22,5 +22,13 @@ public class UserController
     @GetMapping("/{id}")
     public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId){
         return userService.getUserWithDepartment(userId);
+    }
+
+    @Value("${welcome}")
+    private String welcome;
+
+    @GetMapping("/")
+    String helloWorld(){
+        return welcome;
     }
 }
